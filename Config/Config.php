ff1a5d0a4324791dd96419e2900d882f1ea9fed0
ingeniero26 <ingjerson2014@gmail.com>
2022-07-30 13:33:51 -0500

    Empezando tienda online

---
+++ Config/Config.php
@@ -0,0 +1,24 @@
+<?php 
+
+const BASE_URL ="http://localhost/tienda_virtual/";
+
+const HOST ="localhost";
+
+const USER = "root";
+
+const PASS ="";
+
+const DB ="carrito";
+
+const CHARSET ="charset=utf8";
+
+const TITLE ="TIENDA ONLNE";
+
+const MONEDA ="USD";
+
+const CLIENTE_ID ="";
+
+
+
+
+ ?>
