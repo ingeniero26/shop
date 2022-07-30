5570591b84b9beb5391ff28618dc57ded4bde5f0
ingeniero26 <ingjerson2014@gmail.com>
2022-07-30 13:49:16 -0500

    carpetas principales

---
+++ Views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TIENDA ONLINE</title>
+</head>
+<body>
+	<h1>MI TIENDA ONLINE</h1>
+</body>
+</html>
